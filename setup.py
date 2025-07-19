52517f3ea19d06254acaf1be9a3f4c2800a91298
Chandan Y S <chandan@briisk.io>
2025-07-19 16:47:11 +0530

    Add initial project structure and files

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="llmops-workshop-document-portal",
+    author="Chandan Y S",
+    version="0.1",
+    packages=find_packages(), # Whichever the folder has __init__.py, will be considered as a package
+)
