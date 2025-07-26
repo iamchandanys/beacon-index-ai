25244fbd1f7646758dce0c0d55cce531605cbee4
Chandan Y S <chandan@briisk.io>
2025-07-26 16:11:49 +0530

    Implement CustomException class with logging and add example usage in a new notebook

---
+++ exception/custom_exception.py
@@ -0,0 +1,42 @@
+import sys
+import traceback
+
+from logger.custom_logger import CustomLogger
+
+class CustomException(Exception):
+    def __init__(self, error_message: str, error_details: sys):
+        _,_,exc_traceback = error_details.exc_info()
+        self.file_name = exc_traceback.tb_frame.f_code.co_filename
+        self.line_number = exc_traceback.tb_lineno
+        self.eror_message = error_message
+        self.traceback_str = ''.join(traceback.format_exception(*error_details.exc_info()))
+        
+        self.logger = CustomLogger().get_logger(__file__)
+
+    def __str__(self):
+        self.logger.error(
+            f"File: {self.file_name}, Line: {self.line_number}, "
+            f"Message: {self.eror_message}, Traceback: {self.traceback_str}"
+        )
+        
+        return (
+            f"\n--- Custom Exception ---\n"
+            f"File      : {self.file_name}\n"
+            f"Line      : {self.line_number}\n"
+            f"Message   : {self.eror_message}\n"
+            f"Traceback :\n{self.traceback_str}"
+        )
+        
+# Example usage of the CustomException
+if __name__ == "__main__":
+    try:
+        # Simulate an error
+        1 / 0
+    except Exception as e:
+        custom_exception = CustomException(str(e), sys)
+        
+        customLogger = CustomLogger()
+        logger = customLogger.get_logger(__file__)
+        logger.error(custom_exception)
+        
+        raise custom_exception
