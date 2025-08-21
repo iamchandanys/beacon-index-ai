e7320c6b1ca2534046c45c086f46777f01030ef9
Chandan Y S <chandan@briisk.io>
2025-08-21 22:17:56 +0530

    Add additional Azure configuration settings to app_settings.py
    - Introduced new environment variables for Azure OpenAI and Content Safety services.
    - Reordered existing variables for better organization.

--- src/core/app_settings.py
+++ src/core/app_settings.py
@@ -4,9 +4,13 @@ class Settings(BaseSettings):
     """
     Loads application configuration from environment variables or a .env file using Pydantic.
     """
-    AZURE_STORAGE_CONN_STR: str
+   
     AZURE_OPENAI_API_KEY: str
     AZURE_OPENAI_ENDPOINT: str
+    AZURE_OPENAI_GPT_4O_FULL_ENDPOINT: str
+    AZURE_CONTENT_SAFETY_ENDPOINT: str
+    AZURE_CONTENT_SAFETY_KEY: str
+    AZURE_STORAGE_CONN_STR: str
     AZURE_MONITOR_CONN_STR: str
     AZ_EMC_COSMOS_DB_CONNECTION_STRING: str
     AZ_EMC_COSMOS_DB_SITES_DATABASE_NAME: str
